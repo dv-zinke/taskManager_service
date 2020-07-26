632aad8b0b29071f70501e4b00c52df930b0f8f5
hj <dv.zinke@gmail.com>
2020-07-27 00:56:57 +0900

    feat : 몽고 db 연결 및 환경 설정을 위한 dotenv 설치

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
+const dotEnv = require("dotenv");
 const app = express();
+dotEnv.config();
 
 
-app.listen(4000, ()=> console.log('part 4000 start'));
+mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.taotb.mongodb.net/taskManager?retryWrites=true&w=majority`)
+    .then(()=>{
+        app.listen(4000, ()=> console.log('part 4000 start'));
+    }).catch((error)=>{
+        console.log(error);
+});
+
