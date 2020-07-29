d230f89512d8869c0b27655581a3d08472485528
hj <dv.zinke@gmail.com>
2020-07-30 00:17:25 +0900

    feat : body-parser 추가, post 추가, node 버전 변경으로인한 import방식으로 변경

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const dotEnv = require("dotenv");
-const app = express();
-dotEnv.config();
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+dotenv.config();
 
+
+const app = express();
+
+app.use(bodyParser.json())
+app.post('/auth/signup', (req,res,next)=>{
+    console.log(req.body.email);
+    res.json({
+        message: req.body
+    })
+});
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.taotb.mongodb.net/taskManager?retryWrites=true&w=majority`)
     .then(()=>{
